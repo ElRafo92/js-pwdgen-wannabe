cad13b8e86ea33e53533a0fa9f585002df179c27
ElRafo92 <rafael.martinez@outlook.it>
2022-10-19 16:40:35 +0200

    aggiunti commenti file js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// dichiarazione variabili di input
 const Name =  prompt (`Inserisci il tuo nome:`)
 const Surname =  prompt (`Inserisci il tuo cognome:`)
 const FavouriteColor =  prompt (`Inserisci il tuo colore preferito:`)
+
+// dichiarazione per la nuova password
 const newPassword = `${Name}${Surname}${FavouriteColor}76`
 
 document.getElementById(`psswrd`).innerHTML = newPassword;
